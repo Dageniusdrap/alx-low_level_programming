1275019120785a5639d236e21c4fb7d3cae1c825
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-01 16:57:04 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/**
+ * main - Starting point.
+ *
+ * Return: Always 0 for success mesaage;
+ */
+
+int main(void)
+{
+	char low, e, q;
+
+	e = "e";
+
+	q = "q";
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+		{
+		putchar(low);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
