acaeb682ddd1a422f37384bc3cf0caad43b17fc2
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-02 12:57:36 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Starting point
+ *
+ * Return: Always 0 show success.
+ */
+
+int main(void)
+{
+	int k, d;
+
+	for (k = '0'; k < '9'; k++)
+	{
+	for (k = d + 1; d <= '9'; d++)
+	{
+	if (d != k)
+	{
+	putchar(k);
+	putchar(d);
+	if (k == '9' && d == '8')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
