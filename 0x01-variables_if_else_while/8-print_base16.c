e30dad271cd4b6aa021ff42139c9538ea01cfe74
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-01 17:48:01 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Starting Point.
+ *
+ * Return: Success message
+ */
+
+int main(void)
+{
+	int k;
+
+	char low;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+	putchar(k);
+	}
+
+	for (low = 'a'; low <= 'f'; low++)
+	{
+	putchar(low)
+	}
+	putchar('\n');
+	return (0);
+}
