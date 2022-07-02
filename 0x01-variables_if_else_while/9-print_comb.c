0294f12563b16c79c2312e282267c46b8e985be4
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-01 18:03:40 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Starting point of the project.
+ *
+ * Return: Project ran successfully.
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+	putchar(d);
+		if (d != '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
