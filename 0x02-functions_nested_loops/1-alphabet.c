ffec1b92098cfc4f392c7d1eb64b1148da6434a9
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-05 14:04:11 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,18 @@
  * Return: Always 0.
  */
 
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 	_putchar(i);
-	_putchar(i);
 	}
+}	_putchar('\n');
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
