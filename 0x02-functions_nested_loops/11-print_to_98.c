e156c3ab17feea6637226ed4e46f3e0e55fd5765
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-05 17:03:35 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,16 @@ void print_to_98(int n)
 	{
 	while (n > 98)
 	{
-	printf("%d, ", n--);
+	_putchar("%d, ", n--);
 	}
-	printf("%d\n", n);
+	_putchar("%d\n", n);
 	}
 	else
 	{
 	while (n < 98)
 	{
-	printf("%d, ", n++);
+	_putchar("%d, ", n++);
 	}
-	printf("%d\n", n);
+	_putchar("%d\n", n);
 	}
 }
