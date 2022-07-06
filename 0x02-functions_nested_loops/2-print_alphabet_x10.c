fd7c66d4260295e047da77485ded9cea6c33c42c
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-05 17:05:38 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Starting Point
- *
- * Return: Always 0
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 
 void print_alphabet_x10(void)
