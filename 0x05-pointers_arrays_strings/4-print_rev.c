1ec357460cefb6244155ef31ec94ecdc4b7310dc
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-11 12:51:09 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+
+void print_rev(char *s)
+{
+	int k, index;
+
+	while (s[index++])
+	{
+	k++;
+	}
+	for (index = k - 1; index >= 0; index--)
+	{
+	_putchar(s[index]);
+	}
+	_putchar('\n');
+}
