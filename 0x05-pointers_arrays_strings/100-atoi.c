45a586454d065cc47216ad82a430f669d4dbbc90
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-12 10:18:35 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value of the converted string.
+ */
+
+int _atoi(char *s)
+{
+	int set_sign = 1;
+	unsigned int numb = 0;
+
+	do {
+	if (*s == '-')
+	{
+	set_sign *= -1
+	}
+	else if (*s >= '0' && *s <= '9')
+	{
+	numb = (numb * 10) + (*s - '0');
+	}
+	else if (numb > 0)
+	{
+	break;
+	}
+	}
+	while (*s++)
+	{
+	}
+	return (numb * set_sign);
+}
