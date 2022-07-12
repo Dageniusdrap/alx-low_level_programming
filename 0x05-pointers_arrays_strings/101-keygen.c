a96d9db33d1c90a5b992b6cadc7bc9b4b018c2e4
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-12 11:09:28 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -37,13 +37,13 @@ int main(void)
 	password[index] -= diff_half1;
 	break;
 	}
+	}
 	for (index = 0; password[index]; index++)
 	{
 	if (password[index] >= (33 + diff_half2))
 	{
 	password[index] -= diff_half2;
 	break;
-	}
 	}
 	}
 	}
