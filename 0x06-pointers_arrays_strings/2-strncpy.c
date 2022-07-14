bbb69189a4cc121daa1cc92721e974436e683ac7
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-07-14 09:06:41 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -22,7 +22,7 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 	dest[index] = src[index];
 	}
-	for (index = src_len; index < n; index++)
+	for (index = copy_len; index < n; index++)
 	{
 	dest[index] = '\0';
 	}
