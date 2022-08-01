e11bc7fe020171fe928c7a19d597eb4c23915d1a
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-08-01 11:37:43 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -15,10 +15,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d == NULL)
 	{
+	return;
+	}
 	d->name = name;
 	d->age = age;
 	d->onwer = owner;
-	}
 }
