3eadaaaf46b91a900b9201d49bef9cb97aa4b02b
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-08-01 14:19:07 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,12 +14,13 @@ dog_t *new_dog(char *name, float age, char *owner);
 
 int _strlen(char *str)
 {
-  int len = 0;
+	int len = 0;
 
-  while (*str++)
-    len++;
-
-  return (len);
+	while (*str++)
+	{
+	len++
+	}
+	return (len);
 }
 
 /**
@@ -33,14 +34,14 @@ int _strlen(char *str)
 
 char *_strcopy(char *dest, char *src)
 {
-  int index = 0;
+	int index = 0;
 
-  for (index = 0; src[index]; index++)
-    dest[index] = src[index];
-
-  dest[index] = '\0';
-
-  return (dest);
+	for (index = 0; src[index]; index++)
+	{
+	dest[index] = src[index];
+	}
+	dest[index] = '\0';
+	return (dest);
 }
 
 /**
@@ -54,32 +55,32 @@ char *_strcopy(char *dest, char *src)
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  dog_t *doggo;
+	dog_t *doggo;
 
-  if (name == NULL || age < 0 || owner == NULL)
-  {
-  return  (NULL);
-  }
-  doggo = malloc(sizeof(dog_t));
-  if (doggo == NULL)
-  {
-  return (NULL);
-  }
-  doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-  if (doggo->name == NULL)
-    {
-      free(doggo);
-      return (NULL);
-    }
-  doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-  if (doggo->owner == NULL)
-    {
-      free(doggo->name);
-      free(doggo);
-      return (NULL);
-    }
-  doggo->name = _strcopy(doggo->name, name);
-  doggo->age = age;
-  doggo->owner = _strcopy(doggo->owner, owner);
-  return (doggo);
+	if (name == NULL || age < 0 || owner == NULL)
+	{
+	return (NULL);
+	}
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+	{
+	return (NULL);
+	}
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo->name == NULL)
+	{
+	free(doggo);
+	return (NULL);
+	}
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
+	{
+	free(doggo->name);
+	free(doggo);
+	return (NULL);
+	}
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner, owner);
+	return (doggo);
 }
