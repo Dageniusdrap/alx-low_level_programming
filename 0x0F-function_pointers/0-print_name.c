01483d7326f125b6983c975b199868ac7972365a
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-08-03 09:52:58 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,11 @@
+#include <stdlib>
 #include "function_pointers.h"
 
 /**
  * print_name - Prints a name.
+ *
  * @name: The name to be printed.
+ *
  * @f: A pointer to a function that prints a name.
  */
 
