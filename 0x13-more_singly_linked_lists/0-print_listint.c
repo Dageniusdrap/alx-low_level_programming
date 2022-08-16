998577e3d22fd193f5f2d3bf063243ca362fb584
Dradriga Patrick <dradrigapatrick@gmail.com>
2022-08-16 09:36:48 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,21 +11,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-	unsigned int nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-	printf("[%u] ", h->len);
-	if (h->str == NULL)
-	{
-	printf("(nil)\n");
-	}
-	else
-	{
-	printf("%s\n", h->str);
-	}
-	h = h->next;
 	nodes++;
+	printf("%d\n", h->n);
+	h = h->next;
 	}
 	return (nodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,51 +2,32 @@
 #define LISTS_H
 
 
-
 /*
-
  * File: lists.h
-
  * Auth: Dradriga Patrick
-
  * Desc: Header file containing prototypes and definitions for all functions
-
  *       and types written in the 0x12-more_singly_linked_lists directory.
-
  */
-
-
 
 #include <stdlib.h>
 
-
-
 /**
-
  * struct listint_s - singly linked list
-
  * @n: integer
-
  * @next: points to the next node
-
  *
-
  * Description: singly linked list node structure
-
- * for Holberton project
-
+ *
  */
 
 typedef struct listint_s
 
 {
 
-	int n;
-
-	struct listint_s *next;
+	    int n;
+	    struct listint_s *next;
 
 } listint_t;
-
 
 
 size_t print_listint(const listint_t *h);
